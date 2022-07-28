309c27e58417500670a595965a0283b7ce106b0c
Ana Prahova <anastasiaprahova@gmail.com>
2022-07-28 14:40:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let ball = document.getElementById('float-circle');
 
 // Write your code below
-const up = () => ball.style.bottom = '500px'
+const up = () => ball.style.bottom = '460px'
 const down = ()=> ball.style.bottom = '50px'
 
 document.addEventListener('keydown', up);
